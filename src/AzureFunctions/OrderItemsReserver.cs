5d071a649647636d9b47ce3b6164d2c8f8fcf695
Kiryl_Ramashkevich <Kiryl_Ramashkevich@epam.com>
2022-10-27 21:33:42 +0300

    add logic for sending order details to azure blob storage in OrderItemsReserver

--- src/AzureFunctions/OrderItemsReserver.cs
+++ src/AzureFunctions/OrderItemsReserver.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Azure.WebJobs;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
 namespace AzureFunctions
@@ -6,12 +11,19 @@ namespace AzureFunctions
     public static class OrderItemsReserver
     {
         [FunctionName("order-items-reserver")]
-        public static void Run(
+        public static async Task Run(
             [ServiceBusTrigger("orders", Connection = "ServiceBusConnection")]
             string myQueueItem,
             ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            var blobServiceClient = new BlobServiceClient(
+                Environment.GetEnvironmentVariable("BlobConnectionString")
+            );
+            var containerClient = blobServiceClient.GetBlobContainerClient("e-shop-orders");
+            var blobName = "order-details." + Guid.NewGuid() + ".json";
+            await using MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(myQueueItem));
+            await containerClient.UploadBlobAsync(blobName, ms);
         }
     }
 }
